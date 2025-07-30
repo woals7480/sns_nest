664378f1850d0b3925535e3b0d3068e088b6d549
woals7480 <94383932+woals7480@users.noreply.github.com>
2025-07-30 16:31:33 +0900

    Service로 로직 옮기기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,56 +3,11 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
-
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_official',
-    title: '뉴진스 민지',
-    content: '메이크업 고치고 있는 민지',
-    likeCount: 10000,
-    commentCount: 99999,
-  },
-  {
-    id: 2,
-    author: 'newjeans_official',
-    title: '뉴진스 하니',
-    content: '메이크업 고치고 있는 하니',
-    likeCount: 1003,
-    commentCount: 99998,
-  },
-  {
-    id: 3,
-    author: 'newjeans_official',
-    title: '뉴진스 해린',
-    content: '메이크업 고치고 있는 해린',
-    likeCount: 10000,
-    commentCount: 99999,
-  },
-  {
-    id: 4,
-    author: 'newjeans_official',
-    title: '뉴진스 재민',
-    content: '메이크업 고치고 있는 재민',
-    likeCount: 1003,
-    commentCount: 99998,
-  },
-];
 
 @Controller('posts')
 export class PostsController {
@@ -61,19 +16,13 @@ export class PostsController {
   // 전체 posts
   @Get()
   getPosts() {
-    return posts;
+    return this.postsService.getAllPosts();
   }
 
   // 해당 posts
   @Get(':id')
   getPost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    return post;
+    return this.postsService.getPostById(+id);
   }
 
   // post 생성
@@ -83,18 +32,7 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
-      author,
-      title,
-      content,
-      likeCount: 0,
-      commentCount: 0,
-    };
-
-    posts = [...posts, post];
-
-    return post;
+    return this.postsService.createPost(author, title, content);
   }
 
   // 해당 post 변경
@@ -105,40 +43,12 @@ export class PostsController {
     @Body('title') title?: string,
     @Body('content') content?: string,
   ) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    if (author) {
-      post.author = author;
-    }
-
-    if (title) {
-      post.title = title;
-    }
-
-    if (content) {
-      post.content = content;
-    }
-
-    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
-
-    return post;
+    return this.postsService.updatePost(+id, author, title, content);
   }
 
   // post 삭제
   @Delete(':id')
   deletePost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    posts = posts.filter((post) => post.id !== +id);
-
-    return id;
+    return this.postsService.deletePost(+id);
   }
 }
