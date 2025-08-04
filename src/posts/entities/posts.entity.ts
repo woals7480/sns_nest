08d7f58d89c64a65aa6ee196dfbc325bf1e8581c
woals7480 <94383932+woals7480@users.noreply.github.com>
2025-08-04 11:35:43 +0900

    basemodel 생성

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,11 +1,9 @@
+import { BaseModel } from 'src/common/entities/base.entity';
 import { UsersModel } from 'src/users/entities/users.entity';
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne } from 'typeorm';
 
 @Entity()
-export class PostsModel {
-    @PrimaryGeneratedColumn()
-    id: number;
-
+export class PostsModel extends BaseModel {
     // UsersModel과 연동
     // null이 되면 안됨
     @ManyToOne(() => UsersModel, (user) => user.posts, {
