7ac8cb1b83f822e06d2c95767de5eb0f1c5ff185
woals7480 <woals7480@gmail.com>
2025-08-14 18:52:47 +0900

    socket guard

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -13,8 +13,15 @@ import { ChatsService } from './chats.service';
 import { EnterChatDto } from './dto/enter-chat.dto';
 import { CreateMessagesDto } from './messages/dto/create-messages.dto';
 import { ChatsMessagesService } from './messages/messages.service';
-import { UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+    UseFilters,
+    UseGuards,
+    UsePipes,
+    ValidationPipe,
+} from '@nestjs/common';
 import { SocketCatchHttpExceptionFilter } from 'src/common/exception-filter/socket-catch-http.exception-filter';
+import { SocketBearerTokenGuard } from 'src/auth/guard/socket/socket-bearer-token.guard';
+import { UsersModel } from 'src/users/entities/users.entity';
 
 @WebSocketGateway({
     // ws://localhost:3000/chats
@@ -43,10 +50,11 @@ export class ChatsGateway implements OnGatewayConnection {
         }),
     )
     @UseFilters(SocketCatchHttpExceptionFilter)
+    @UseGuards(SocketBearerTokenGuard)
     @SubscribeMessage('create_chat')
     async createChat(
         @MessageBody() data: CreateChatDto,
-        @ConnectedSocket() socket: Socket,
+        @ConnectedSocket() socket: Socket & { user: UsersModel },
     ) {
         const chat = await this.chatsService.createChat(data);
     }
--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -8,11 +8,15 @@ import { CommonModule } from 'src/common/common.module';
 import { ChatsMessagesService } from './messages/messages.service';
 import { MessagesModel } from './messages/entities/messages.entity';
 import { MessagesController } from './messages/messages.controller';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([ChatsModel, MessagesModel]),
         CommonModule,
+        AuthModule,
+        UsersModule,
     ],
     controllers: [ChatsController, MessagesController],
     providers: [ChatsGateway, ChatsService, ChatsMessagesService],
